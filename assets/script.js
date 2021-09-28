d84734c002549b75f8ee1e8e3daa8bddc98ab6b7
Chris Zuniga <zunigachristian81@gmail.com>
2021-09-28 15:48:08 -0500

    api fetch got a response

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,19 @@
 //console.log("test");
+// display curent day
 var currentDay = moment();
 $("#currentDay").text(currentDay.format("dddd, MMM Do"));
+
+var searchBtnEl = document.querySelector("#searchCity");
+var inputEl = document.querySelector("#searchInput");
+
+function searchCityApi() {
+    var city = inputEl.value;
+    var apiEndpoint = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=d243b0fe6a40d063276b726c6f2e14e0";
+
+    fetch(apiEndpoint).then(function(response) {
+        console.log(response);
+        //return response.json();
+    })
+}
+
+searchBtnEl.addEventListener("click", searchCityApi);
