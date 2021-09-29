740e84dcf35a8f68813b9028fbb07823a1f5f69d
Chris Zuniga <zunigachristian81@gmail.com>
2021-09-29 12:06:44 -0500

    second api call, response will daily weather

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,7 @@ $("#currentDay").text(currentDay.format("dddd, MMM Do"));
 
 var searchBtnEl = document.querySelector("#searchCity");
 var inputEl = document.querySelector("#searchInput");
-var pEl = document.querySelector("#currentCity");
+//var pEl = document.querySelector("#currentCity");
 
 
 function searchCityApi() {
@@ -16,10 +16,28 @@ function searchCityApi() {
         console.log(response);
         return response.json();
     }).then(function(data){
-        console.log(data);
-        var pSrc = data.main.temp;
-        pEl.setAttribute("src", pSrc);
+        //console.log(data);
+       // console.log(data.main.temp);
+        fiveDays(data);
     })
 }
 
+
+function fiveDays(data){
+    var longitude = data.coord.lon;
+        console.log(longitude);
+
+    var latitude = data.coord.lat;    
+        console.log(latitude);
+
+
+    var fiveDayApi = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=minutely,hourly&appid=d243b0fe6a40d063276b726c6f2e14e0";
+
+    fetch(fiveDayApi).then(function(response) {
+        console.log(response);
+        return response.json();
+    }) .then(function(data2){
+        console.log(data2);
+    })
+}
 searchBtnEl.addEventListener("click", searchCityApi);
